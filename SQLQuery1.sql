9730fed52744fe5649dabef22c0e062c7dc57cfa
Kirill_Sing <kirill.sing2000@gmail.com>
2018-10-04 10:54:07 +0300

    kyky

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿use master
+go
+create database dfjkl
+go
+use dfjkl
+go
+create table (
+id int primary key, 
+test_data char(500)
+)
+insert into testtable(id,test_data) values (1,3)
+go
+select * from testtable
+go
