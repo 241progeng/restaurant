9e91aff5d94a2cb4c77f04a7778cb43b189326e7
Kirill_Sing <kirill.sing2000@gmail.com>
2018-10-08 10:48:54 +0300

    Crush test

---
+++ restaurant/ClientsWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace restaurant
+{
+    public partial class ClientsWindow : Window
+    {
+        public Client Client { get; private set; }
+
+        public ClientsWindow(Client p)
+        {
+            InitializeComponent();
+            Client = p;
+            this.DataContext = Client;
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
--- restaurant/MainWindow.xaml.cs
+++ restaurant/MainWindow.xaml.cs
@@ -1,28 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+//Added for database
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Data.Entity;
+//dfwsadasdsa test stesrfdsfvbpdfalds
 
 namespace restaurant
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        ApplicationContext db;
         public MainWindow()
         {
             InitializeComponent();
+
+            db = new ApplicationContext();
+            db.Clients.Load();
+            this.DataContext = db.Clients.Local.ToBindingList();
         }
+        // добавление
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            ClientsWindow clientWindow = new ClientsWindow(new Client());
+            if (clientWindow.ShowDialog() == true)
+            {
+                Client client = clientWindow.Client;
+                db.Clients.Add(client);
+                db.SaveChanges();
+            }
+        }
+        // редактирование
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            // если ни одного объекта не выделено, выходим
+            if (clientsList.SelectedItem == null) return;
+            // получаем выделенный объект
+            Client client = clientsList.SelectedItem as Client;
+
+            ClientsWindow clientWindow = new ClientsWindow(new Client
+            {
+                Id = client.Id,
+                Discount = client.Discount,
+                Name = client.Name
+            });
+
+            if (clientWindow.ShowDialog() == true)
+            {
+                // получаем измененный объект
+                client = db.Clients.Find(clientWindow.Client.Id);
+                if (client != null)
+                {
+                    client.Discount = clientWindow.Client.Discount;
+                    client.Name = clientWindow.Client.Name;
+                    client.Id = clientWindow.Client.Id;
+                    db.Entry(client).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+        }
+        // удаление
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // если ни одного объекта не выделено, выходим
+            if (clientsList.SelectedItem == null) return;
+            // получаем выделенный объект
+            Client client = clientsList.SelectedItem as Client;
+            db.Clients.Remove(client);
+            db.SaveChanges();
+        }
+    }
+    public class Client : INotifyPropertyChanged
+    {
+        private string name;
+        private int discount;
+        private int id;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public int Discount
+        {
+            get { return discount; }
+            set
+            {
+                discount = value;
+                OnPropertyChanged("Discount");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
     }
 }
