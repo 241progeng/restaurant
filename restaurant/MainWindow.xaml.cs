8fa8ec5c9e8499ed92e515603bc6b1e45ae56fe6
metahet <metahet@ukr.net>
2018-11-08 19:00:43 +0200

    1

--- restaurant/MainWindow.xaml.cs
+++ restaurant/MainWindow.xaml.cs
@@ -18,19 +18,66 @@ namespace restaurant
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+  
     public partial class MainWindow : Window
+
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("print");
+            TaskWindow taskWindow = new TaskWindow();
+            taskWindow.Show();
         }
         private void Button_Click1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("товар");
+            MessageBox.Show("товар1");
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар2");
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар3");
+        }
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар4");
+        }
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар5");
+        }
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар6");
+        }
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар7");
+        }
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар8");
+        }
+        private void Button_Click9(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар9");
+        }
+        private void Button_Click10(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар10");
+        }
+        private void Button_Click11(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар11");
+        }
+        private void Button_Click12(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("товар12");
         }
     }
 }
---
+++ restaurant/TaskWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace restaurant
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class TaskWindow : Window
+    {
+        public TaskWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено accept");
+        }
+
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено esc");
+            this.Close(); // закрытие окна
+        }
+    }
+}
