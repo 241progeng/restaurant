feeb72c98a1a6f18b23c0d997f1a71e14162d00f
Kirill_Sing <kirill.sing2000@gmail.com>
2018-11-08 19:16:36 +0200

    New

--- restaurant/EnterClient.xaml.cs
+++ restaurant/EnterClient.xaml.cs
@@ -5,46 +5,46 @@ using System.Data.Entity;
 namespace restaurant
 {
 
-    public partial class EnterClient : Window
-    {
-        ApplicationContext db;
+    //public partial class EnterClient : Window
+    //{
+    //    ApplicationContext db;
 
-        public string Discount { get; private set; }
+    //    public string Discount { get; private set; }
 
-        public EnterClient()
-        {
-            InitializeComponent();
+    //    public EnterClient()
+    //    {
+    //        InitializeComponent();
 
-            db = new ApplicationContext();
-            db.Clients.Load();
-            this.DataContext = db.Clients.Local.ToBindingList();
-        }
-        private void Cancel_Click(object sender, RoutedEventArgs e)
-        {
-            // this.DialogResult = true;
-            Close();
-        }
-        private void Accept_Click(object sender, RoutedEventArgs e)
-        {
-            string em = EmailBox.Text;
-            string pw = PasswordBox.Password;
+    //        db = new ApplicationContext();
+    //        db.Clients.Load();
+    //        this.DataContext = db.Clients.Local.ToBindingList();
+    //    }
+    //    private void Cancel_Click(object sender, RoutedEventArgs e)
+    //    {
+    //        // this.DialogResult = true;
+    //        Close();
+    //    }
+    //    private void Accept_Click(object sender, RoutedEventArgs e)
+    //    {
+    //        string em = EmailBox.Text;
+    //        string pw = PasswordBox.Password;
 
-            var clients = (from c in db.Clients where c.Email == em select c).ToList();
+    //        var clients = (from c in db.Clients where c.Email == em select c).ToList();
 
-            if (clients.Count() == 0)
-            {
-                MessageBox.Show("Error");
+    //        if (clients.Count() == 0)
+    //        {
+    //            MessageBox.Show("Error");
 
-            }
-            else
-            {
-                if (pw == clients.FirstOrDefault<Client>().Password)
-                {
-                    MessageBox.Show("Discount");
-                }
-            }
+    //        }
+    //        else
+    //        {
+    //            if (pw == clients.FirstOrDefault<Client>().Password)
+    //            {
+    //                MessageBox.Show("Discount");
+    //            }
+    //        }
 
-        }
-    }
+    //    }
+    //}
 
 }
